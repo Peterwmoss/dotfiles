e9e3834fa32c2c39f14fd6c58142717c005cf414
Peter K. Moss <peter@moss.dk>
2022-12-17 12:10:28 +0100

    2

--- config/pvim/config.lua
+++ config/pvim/config.lua
@@ -111,37 +111,4 @@ pvim.custom_autocommands = {
 }
 
 pvim.custom_init.dap = function()
-  local dap = require "dap"
-
-  -- Node adapter config
-  dap.adapters.node2 = {
-    type = 'executable',
-    command = 'node',
-    args = {os.getenv('HOME') .. '/work/vscode-node-debug2/out/src/nodeDebug.js'},
-  }
-
-  -- Javascript
-  dap.configurations.javascript = {
-    {
-      type = 'node2',
-      request = 'launch',
-      program = '${file}',
-      cwd = vim.fn.getcwd(),
-      sourceMaps = true,
-      protocol = 'inspector',
-      console = 'integratedTerminal',
-    },
-  }
-
-  -- Typescript
-  dap.configurations.typescript = {
-    {
-      type = "node2",
-      request = "attach",
-      program = "${file}",
-      cwd = vim.fn.getcwd(),
-      sourceMaps = true,
-      protocol = "inspector"
-    }
-  }
 end
--- config/zsh/.zshrc
+++ config/zsh/.zshrc
@@ -115,6 +115,8 @@ export ZDOTDIR="${ZDOTDIR:=$XDG_CONFIG_HOME/zsh}"
 source "$ZDOTDIR/.zshenv"
 source "${ZDOTDIR:-$HOME}/zprezto/init.zsh"
 
+export GOBIN="$HOME/.local/bin"
+
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
