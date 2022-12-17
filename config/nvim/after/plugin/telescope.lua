5c1069d4bcbeb8e5836eda27c467340fe1bbcc01
Peter K. Moss <peter@moss.dk>
2022-12-17 15:38:14 +0100

    hidden files in find_files

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,7 @@
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<C-p>', builtin.commands)
-vim.keymap.set('n', '<leader>f', builtin.find_files)
+vim.keymap.set('n', '<leader>f', function() builtin.find_files({ hidden = true }) end)
 
 vim.keymap.set('n', '<leader>th', builtin.help_tags)
 vim.keymap.set('n', '<leader>ts', builtin.grep_string)
