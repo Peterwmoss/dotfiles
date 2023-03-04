521b56140af061961a3a8dc10e13b085c4052578
Peter K. Moss <peter@moss.dk>
2023-03-04 14:49:50 +0100

    config

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -3,38 +3,38 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.ensure_installed({
-	'tsserver',
-	'dockerls',
-	'gopls',
-	'angularls',
-	'bashls',
-	'cmake',
-	'cssls',
-	'gradle_ls',
-	'html',
-	'jedi_language_server',
-	'jsonls',
-	'marksman',
-	'texlab',
-	'volar',
-	'yamlls',
+  'tsserver',
+  'dockerls',
+  'gopls',
+  'angularls',
+  'bashls',
+  'cmake',
+  'cssls',
+  'gradle_ls',
+  'html',
+  'jedi_language_server',
+  'jsonls',
+  'marksman',
+  'texlab',
+  'volar',
+  'yamlls',
 })
 
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	['<C-i>'] = cmp.mapping.complete(),
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<C-i>'] = cmp.mapping.complete(),
 })
 
 lsp.setup_nvim_cmp({
-	mapping = cmp_mappings
+  mapping = cmp_mappings
 })
 
 lsp.on_attach(function(_, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "ga", vim.lsp.buf.code_action, opts)
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
@@ -52,3 +52,12 @@ lsp.on_attach(function(_, bufnr)
 end)
 
 lsp.setup()
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
